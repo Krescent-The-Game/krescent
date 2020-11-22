63b73487fbd89f9d39fb56f5b2083a5dd8bbeda5
m3yevn <mmm.myintmyat@gmail.com>
2020-11-23 00:15:19 +0800

    fix: bullet collision

--- utils/Bullet.helper.js
+++ utils/Bullet.helper.js
@@ -1,7 +1,7 @@
 import * as BABYLON from "@babylonjs/core";
 
 export const createBullets = (handleIntersect) => (buggy) => {
-  const bullet = new BABYLON.MeshBuilder.CreateCylinder(
+  let bullet = new BABYLON.MeshBuilder.CreateCylinder(
     "bullet",
     {
       diameter: 0.015,
@@ -19,27 +19,25 @@ export const createBullets = (handleIntersect) => (buggy) => {
   material.diffuseColor = new BABYLON.Color3(1.0, 0.2, 0.7);
   bullet.material = material;
 
+  window.bulletTimer = setInterval(() => {
+    clearInterval(window.bulletTimer);
+
+    bullet.position.x += 0.1;
+    if (bullet.position.x > 5) {
+      bullet.dispose();
+      bullet = null;
+    }
+  }, 100);
+
   buggy.getScene().registerBeforeRender(() => {
     const scene = buggy.getScene();
     const bombs = scene.meshes.filter((v) => v.name === "Bomb");
     bombs.forEach((bomb) => {
-      if (bomb.intersectsMesh(bullet, false)) {
+      if (!!bullet && bullet.intersectsMesh(bomb, true)) {
         bomb.dispose();
         bullet.dispose();
         handleIntersect();
       }
     });
   });
-
-  setInterval(
-    () =>
-      requestAnimationFrame(() => {
-        bullet.position.x += 0.1;
-      }),
-    100
-  );
-
-  setTimeout(() => {
-    bullet.dispose();
-  }, 4000);
 };
